1657026953efe9de4d70f396e902e2e7e9de9b6f
fan hong <fyh_4576@126.com>
2020-11-26 13:12:24 +0800

    vue模板工程初始创建

---
+++ README.md
@@ -0,0 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-25 16:38:31
+ * @LastEditTime: 2020-11-26 13:08:54
+ * @LastEditors: your name
+ * @Description: In User Settings Editf
+ * @FilePath: /fyh-vue-temp-master/README.md
+-->
+# -vue-temp
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-25 16:38:31
+ * @LastEditTime: 2020-11-26 13:09:11
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /fyh-vue-temp-master/src/App.vue
+-->
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <div>
+      <router-link to="/home">首页</router-link>
+      <router-link to="/login">登录</router-link>
+    </div>
+    <img alt="Vue logo" src="./assets/img/logo.png">
+    <HelloWorld msg="a fyh vue template"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from 'components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created: function() {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
